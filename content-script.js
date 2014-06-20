c0e86d0f77ffee13912c1c2d5fa0fbf882e5841d
David Mzareulyan <david@hiero.ru>
2014-06-20 13:36:54 +0400

    Показ номера версии, ссылка на GitHub

--- content-script.js
+++ content-script.js
@@ -138,7 +138,8 @@
 
         var box = document.createElement("DIV");
         box.className = "box";
-        box.innerHTML = '<div class="box-bar ffnco">\n    <div class="box-corner"></div>\n    <div class="box-bar-text">&nbsp;</div>\n</div>\n<div class="box-body">\n    <ul>\n        <li><a href="#">Настройки FF&amp;Co</a></li>\n    </ul>\n</div>\n<div class="box-bottom">\n    <div class="box-corner"></div>\n    <div class="box-spacer"></div>\n</div>\n        ';
+        box.innerHTML = '<div class="box-bar ffnco">\n    <div class="box-corner"></div>\n    <div class="box-bar-text">&nbsp;</div>\n</div>\n<div class="box-body">\n    <ul>\n        <li>\n            <a href="#">Настройки FF&amp;Co</a>\n            <div class="updated">v {{VERSION}}</div>\n        </li>\n    </ul>\n</div>\n<div class="box-bottom">\n    <div class="box-corner"></div>\n    <div class="box-spacer"></div>\n</div>\n        '
+            .replace("{{VERSION}}", chrome.runtime.getManifest().version);
         box.querySelector("a").onclick = function (e) {
             e.preventDefault();
             window.open(chrome.extension.getURL("options.html"));
