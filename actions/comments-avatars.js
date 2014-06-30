0318bed8b776f8ab73144c2456ad93c1f4fc7b6f
David Mzareulyan <david@hiero.ru>
2014-06-30 21:51:20 +0400

    Другой алгоритм работы с аватарками, ест меньше памяти

--- actions/comments-avatars.js
+++ actions/comments-avatars.js
@@ -1,47 +1,40 @@
 (function () {
     registerAction(function (node) {
         if (!settings["replyLinks"] || !settings["withAvatars"]) return;
+        if (node === undefined) {
+            var styleEl = document.head.appendChild(document.createElement('style'));
+            styleEl.type = 'text/css';
+            styleEl.title = 'avatars';
+            style = styleEl.sheet;
+            console.log(styleEl, style)
+        }
         node = node || document.body;
 
         toArray(node.querySelectorAll(".entry:not(.with-avatars)"))
             .forEach(function (node) { node.classList.add("with-avatars"); });
 
-        toArray(node.querySelectorAll(".comment:not(.ffco-with-avatar) .quote"))
+        toArray(node.querySelectorAll(".comments > .comment:not(.ffco-with-avatar) .quote"))
             .forEach(function (node) {
                 var comment = node.parentNode;
                 comment.classList.add("ffco-with-avatar");
                 var login = comment.dataset["author"];
-                if (login) {
-                    getAvatar(login, function (url) { node.style.backgroundImage = "url(" + url + ")"; });
+                if (login && !(login in avatars)) {
+                    avatars[login] = true;
+                    fetchAvatar(login);
                 }
             });
     });
 
-    var avatars = {};
-    var getAvatar = function (login, callback) {
-        if (login in avatars) {
-            var a = avatars[login];
-            if (a.url) {
-                callback(a.url);
-            } else {
-                a["callbacks"].push(callback);
-            }
-        } else {
-            avatars[login] = {url: null, callbacks: [callback]};
-            fetchAvatar(login, function (login, url) {
-                avatars[login]["callbacks"].forEach(function (cb) { cb(url); });
-                avatars[login] = {url: url};
-            });
-        }
-    };
-    // Эта функция вызывается по одному разу для каждого логина
-    var fetchAvatar = function (login, callback) {
+    var style = null,
+        avatars = {};
+
+    var fetchAvatar = function (login) {
         var a = document.querySelector("a[href='/" + login + "']");
         if (a) {
             var img = document.querySelector("img[src*='" + a.getAttribute("sid") + "']");
             if (img) {
                 var url = img.src.replace(/-(\w+)-(\d+)$/, "-small-$2");
-                callback(login, url);
+                style.insertRule(".comment-from-" + login + " > .quote { background-image: url(" + url + "); }", 0);
                 return;
             }
         }
@@ -50,7 +43,9 @@
         xhr.responseType = "blob";
         xhr.onload = function () {
             if (this.status != 200) return;
-            callback(login, URL.createObjectURL(this.response));
+            var url = URL.createObjectURL(this.response);
+            style.insertRule(".comment-from-" + login + " > .quote { background-image: url(" + url + "); }", 0);
+            setTimeout(function () { URL.revokeObjectURL(url); }, 1000);
         };
         xhr.send();
     };
