5bfa1b22cfce8e69be3e03e09ae3d07e8e114b84
David Mzareulyan <david@hiero.ru>
2014-06-30 11:56:35 +0400

    В попапе показываем оригинальное имя

--- actions/norm-names.js
+++ actions/norm-names.js
@@ -5,6 +5,15 @@ registerAction(function (node) {
     toArray(node.querySelectorAll(".content > .l_profile, .lbody > .l_profile, .name > .l_profile, .foaf > .l_profile"))
         .forEach(function (node) {
             var login = node.getAttribute("href").substr(1);
-            if (node.innerHTML != login) node.innerHTML = login;
+            if (node.innerHTML != login) {
+                node.dataset["displayName"] = node.innerHTML;
+                node.innerHTML = login;
+            }
+        });
+
+    // В попапе показываем оригинальное имя
+    toArray(node.querySelectorAll("#popup .name > .l_profile"))
+        .forEach(function (node) {
+            node.innerHTML = node.dataset["displayName"];
         });
 });
